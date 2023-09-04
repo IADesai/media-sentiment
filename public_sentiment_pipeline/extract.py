bf1cbe9d1cae0f6f09f763db62905f46dc7d1621
rk145lr <132599493+rk145lr@users.noreply.github.com>
2023-09-04 17:03:24 +0100

    #12 Added extract of title, url and permalink for each page of a subreddit.

--- public_sentiment_pipeline/extract.py
+++ public_sentiment_pipeline/extract.py
@@ -16,6 +16,23 @@ def get_subreddit_json(config: dict) -> dict:
     return response.json()
 
 
+def create_pages_list(reddit_json: dict) -> list[dict]:
+    """Creates a list containing the links for each page."""
+    pages_list = []
+    for page in reddit_json["data"]["data"]:
+        try:
+            page_dict = {}
+            page_dict["title"] = page["title"]
+            page_dict["subreddit_url"] = page["url"]
+            page_dict["article_url"] = page["article_url"]
+            pages_list.append(page_dict)
+        except AttributeError:
+            print("Missing attribute. Skipping entry.")
+    return pages_list
+
+
 if __name__ == "__main__":  # pragma: no cover
     configuration = dotenv_values()
     reddit_json = get_subreddit_json(configuration)
+    list_of_json = create_pages_list(reddit_json)
+    print(list_of_json)
