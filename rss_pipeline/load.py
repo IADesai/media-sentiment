28b719654146030ae419cd417577f78f52cc08fc
ramiz <ramizalipir76@gmail.com>
2023-09-05 13:27:42 +0100

    created rss load and rds schema

---
+++ rss_pipeline/load.py
@@ -0,0 +1,67 @@
+'''Extracts data from the rss news articles and populates the media-sentiment relational database (rds)'''
+from os import environ
+
+
+from dotenv import load_dotenv
+import pandas as pd
+from psycopg2 import errors, extensions, extras
+import psycopg2
+
+
+def db_connection() -> psycopg2.extensions.connection | None:
+    '''Establish connection with the media-sentiment rds'''
+    try:
+        return psycopg2.connect(dbname=environ["DATABASE_NAME"],
+                                user=environ["DATABASE_USERNAME"],
+                                host=environ["DATABASE_ENDPOINT"],
+                                password=environ["DATABASE_PASSWORD"],
+                                cursor_factory=psycopg2.extras.RealDictCursor)
+
+    except:
+        raise psycopg2.DatabaseError("Error connecting to database.")
+
+
+def create_dataframe(file_name: str) -> pd.DataFrame:
+    '''Creates a Pandas DataFrame with the contents of the provided csv file'''
+    rss_df = pd.read_csv(file_name)
+    return rss_df
+
+
+def get_source_id(conn: psycopg2.extensions.connection, url) -> str:
+    try:
+        source = url.lstrip('https://www.').split('.co.uk')[0]
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT source_id FROM sources WHERE source_name = (%s);", [source])
+            source_id_dict = cur.fetchone()
+        return source_id_dict['source_id']
+    except Exception as e:
+        print('Error retrieving source')
+        print(e.args)
+
+
+def populate_sources_table(conn: psycopg2.extensions.connection, dataframe: pd.DataFrame) -> None:
+    '''Inserts data into the stories table of the rds'''
+    for index, row in dataframe.iterrows():
+        source_id = get_source_id(conn, row.get('url'))
+        try:
+            with conn.cursor() as cur:
+                values = (source_id, row['title'], row['description'],
+                          row['url'], row['pubdate'], row['sentiment_score'])
+                cur.execute(
+                    "INSERT INTO stories (source_id, title, description, url, pub_date, media_sentiment) VALUES (%s,%s,%s,%s,%s,%s)", values)
+                conn.commit()
+        except psycopg2.errors.UniqueViolation as err:
+            # placeholder but we can discuss how to address dupes (maybe replace original?)
+            print('Duplicate data found:', err)
+        except Exception as err:
+            print('Error populating stories database')
+            print(err.args)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    conn = db_connection()
+    df = create_dataframe('daily_mail_uk_news.csv')
+    populate_sources_table(conn, df)
+    conn.close()
---
+++ setup.sql
@@ -0,0 +1,61 @@
+DROP TABLE IF EXISTS sources CASCADE;
+DROP TABLE IF EXISTS reddit_article CASCADE;
+DROP TABLE IF EXISTS story_keyword_link CASCADE;
+DROP TABLE IF EXISTS reddit_keyword_link CASCADE;
+DROP TABLE IF EXISTS keywords CASCADE;
+DROP TABLE IF EXISTS stories CASCADE;
+
+CREATE TABLE sources(
+    source_id INT GENERATED ALWAYS AS IDENTITY,
+    source_name TEXT UNIQUE,
+    PRIMARY KEY (source_id)
+);
+
+CREATE TABLE stories(
+    story_id BIGINT GENERATED ALWAYS AS IDENTITY,
+    source_id SMALLINT,
+    title TEXT,
+    description TEXT,
+    url TEXT UNIQUE,
+    pub_date TIMESTAMP,
+    media_sentiment FLOAT,
+    PRIMARY KEY (story_id),
+    FOREIGN KEY (source_id) REFERENCES sources(source_id)
+);
+
+CREATE TABLE keywords(
+    keyword_id INT GENERATED ALWAYS AS IDENTITY,
+    keyword TEXT UNIQUE,
+    PRIMARY KEY (keyword_id)
+);
+
+CREATE TABLE reddit_article(
+    re_article_id INT GENERATED ALWAYS AS IDENTITY,
+    re_domain TEXT,
+    re_title TEXT,
+    re_article_url TEXT,
+    re_url TEXT UNIQUE,
+    public_sentiment FLOAT,
+    PRIMARY KEY (re_article_id)
+);
+
+CREATE TABLE reddit_keyword_link(
+    re_link_id INT GENERATED ALWAYS AS IDENTITY,
+    keyword_id INT,
+    re_article_id INT,
+    PRIMARY KEY (re_link_id),
+    FOREIGN KEY (keyword_id) REFERENCES keywords(keyword_id),
+    FOREIGN KEY (re_article_id) REFERENCES reddit_article(re_article_id)
+);
+
+CREATE TABLE story_keyword_link(
+    link_id INT GENERATED ALWAYS AS IDENTITY,
+    keyword_id INT,
+    story_id INT,
+    PRIMARY KEY (link_id),
+    FOREIGN KEY (keyword_id) REFERENCES keywords(keyword_id),
+    FOREIGN KEY (story_id) REFERENCES stories(story_id)
+);
+
+INSERT INTO sources (source_name) VALUES ('bbc');
+INSERT INTO sources (source_name) VALUES ('dailymail');
