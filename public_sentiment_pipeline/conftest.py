766ec1004db5e684fc31d990ec45ed33e65ec333
rk145lr <132599493+rk145lr@users.noreply.github.com>
2023-09-05 14:23:28 +0100

    #12 Added unit tests for get_reddit_access_token().

--- public_sentiment_pipeline/conftest.py
+++ public_sentiment_pipeline/conftest.py
@@ -1,9 +1,17 @@
 """Contains useful properties for the unit tests"""
 
 
-class FakeRequest:
+class FakeGet:
     def __init__(self) -> None:
         self.status_code = 200
 
     def json(self):
         return {"success": True}
+
+
+class FakePost:
+    def __init__(self) -> None:
+        self.status_code = 200
+
+    def json(self):
+        return {"success": True, "access_token": "this is an access token"}
--- public_sentiment_pipeline/test_extract.py
+++ public_sentiment_pipeline/test_extract.py
@@ -6,8 +6,8 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 
-from conftest import FakeRequest
-from extract import get_subreddit_json
+from conftest import FakeGet, FakePost
+from extract import get_subreddit_json, get_reddit_access_token
 
 
 @patch("requests.get")
@@ -16,7 +16,7 @@ def test_non_200_raises_connection_error(fake_get):
     """Tests a ConnectionError is raised if a GET request does not return a 200 status code."""
     configuration = {"REDDIT_TOPIC": "unitedkingdom"}
     access_token = "1234567890"
-    fake_request = FakeRequest()
+    fake_request = FakeGet()
     fake_request.status_code = 404
     fake_get.return_value = fake_request
 
@@ -30,8 +30,39 @@ def test_200_status_code_returns_json(fake_get):
     """Tests JSON data is returned with a 200 status code."""
     configuration = {"REDDIT_TOPIC": "unitedkingdom"}
     access_token = "1234567890"
-    fake_get.return_value = FakeRequest()
+    fake_get.return_value = FakeGet()
 
     res = get_subreddit_json(configuration, access_token)
 
     assert isinstance(res, dict)
+
+
+@patch("requests.post")
+@patch("extract.REDDIT_ACCESS_TOKEN_URL", "https://www.reddit.com/api/v1/access_token")
+def test_non_200_raises_connection_error_access_token(fake_get):
+    """Tests a ConnectionError is raised if a GET request does not return a 200 status code."""
+    configuration = {"REDDIT_CLIENT_SECRET": "54321",
+                     "REDDIT_SECRET_KEY": "12345",
+                     "REDDIT_USERNAME": "12345_54321",
+                     "REDDIT_PASSWORD": "54321_12345"}
+    fake_request = FakePost()
+    fake_request.status_code = 403
+    fake_get.return_value = fake_request
+
+    with pytest.raises(ConnectionError):
+        get_reddit_access_token(configuration)
+
+
+@patch("requests.post")
+@patch("extract.REDDIT_ACCESS_TOKEN_URL", "https://www.reddit.com/api/v1/access_token")
+def test_200_status_code_returns_string_access_token(fake_get):
+    """Tests access token string is returned with a 200 status code."""
+    configuration = {"REDDIT_CLIENT_SECRET": "54321",
+                     "REDDIT_SECRET_KEY": "12345",
+                     "REDDIT_USERNAME": "12345_54321",
+                     "REDDIT_PASSWORD": "54321_12345"}
+    fake_get.return_value = FakePost()
+
+    res = get_reddit_access_token(configuration)
+
+    assert isinstance(res, str)
