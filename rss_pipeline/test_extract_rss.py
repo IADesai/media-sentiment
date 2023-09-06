736cd5eb57663ce50050a40929da2984979792aa
Isaac Van Bueren <isaacvanbueren@hotmail.co.uk>
2023-09-06 16:14:00 +0100

    Tests updated

--- rss_pipeline/test_extract_rss.py
+++ rss_pipeline/test_extract_rss.py
@@ -2,6 +2,13 @@ from unittest.mock import patch, mock_open
 from extract_rss import download_bbc_uk_news_xml, download_daily_mail_uk_news_xml
 
 
+BBC_UK_NEWS_RSS_LINK = "http://feeds.bbci.co.uk/news/uk/rss.xml"
+DAILY_MAIL_UK_NEWS_RSS_LINK = "https://www.dailymail.co.uk/home/index.rss"
+
+BBC_UK_NEWS_XML_FILE_NAME = "bbc_uk_news.xml"
+DAILY_MAIL_UK_NEWS_XML_FILE_NAME = "daily_mail_uk_news.xml"
+
+
 @patch("builtins.open", new_callable=mock_open, create=True)
 def test_download_bbc_uk_news_xml(mock_file_open):
 
@@ -10,11 +17,11 @@ def test_download_bbc_uk_news_xml(mock_file_open):
         fake_response.status_code = 200
         fake_response.content = b"Fake XML Content"
 
-        download_bbc_uk_news_xml()
+        download_bbc_uk_news_xml(BBC_UK_NEWS_RSS_LINK,
+                                 BBC_UK_NEWS_XML_FILE_NAME)
 
-        mock_get.assert_called_once_with(
-            "http://feeds.bbci.co.uk/news/uk/rss.xml", timeout=10)
-        mock_file_open.assert_called_once_with("bbc_uk_news.xml", "wb")
+        mock_get.assert_called_once_with(BBC_UK_NEWS_RSS_LINK, timeout=10)
+        mock_file_open.assert_called_once_with(BBC_UK_NEWS_XML_FILE_NAME, "wb")
         mock_file_open().write.assert_called_once_with(b"Fake XML Content")
 
 
@@ -26,9 +33,11 @@ def test_download_daily_mail_uk_news_xml(mock_file_open):
         fake_response.status_code = 200
         fake_response.content = b"Fake XML Content"
 
-        download_daily_mail_uk_news_xml()
+        download_daily_mail_uk_news_xml(
+            DAILY_MAIL_UK_NEWS_RSS_LINK, DAILY_MAIL_UK_NEWS_XML_FILE_NAME)
 
         mock_get.assert_called_once_with(
-            "https://www.dailymail.co.uk/home/index.rss", timeout=10)
-        mock_file_open.assert_called_once_with("daily_mail_uk_news.xml", "wb")
+            DAILY_MAIL_UK_NEWS_RSS_LINK, timeout=10)
+        mock_file_open.assert_called_once_with(
+            DAILY_MAIL_UK_NEWS_XML_FILE_NAME, "wb")
         mock_file_open().write.assert_called_once_with(b"Fake XML Content")
