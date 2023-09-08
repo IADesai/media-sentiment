dd94464fd7c7d1f16ad0ca646170348c37bdbb4a
Ibrahim Ayub Desai <113534999+IADesai@users.noreply.github.com>
2023-09-08 13:42:28 +0100

    update README

--- send_email_pdf/README.md
+++ send_email_pdf/README.md
@@ -6,6 +6,12 @@ The code in this directory creates a PDF containing charts using the most recent
 
 The following environment variables must be supplied in a `.env` file.
 
+- `DATABASE_USERNAME`
+- `DATABASE_PASSWORD`
+- `DATABASE_IP`
+- `DATABASE_PORT`
+- `DATABASE_NAME`
+- `REPORT_FILE`
 - `ACCESS_KEY`
 - `SECRET_KEY`
 - `EMAIL_SENDER`
--- send_email_pdf/main.py
+++ send_email_pdf/main.py
@@ -46,22 +46,25 @@ def join_all_sql_tables(conn: connection) -> pd.DataFrame:
 
 def join_all_stories_info(conn: connection) -> pd.DataFrame:
     "Function that joins the stories and story sources SQL tables"
-    query ="""SELECT story_id, title, description,url, pub_date, media_sentiment, source_name FROM stories JOIN sources ON stories.source_id = sources.source_id"""
+    query = """SELECT story_id, title, description,url, pub_date, media_sentiment, source_name FROM stories JOIN sources ON stories.source_id = sources.source_id"""
     with conn.cursor() as cur:
         cur.execute(query)
         tuples_list = cur.fetchall()
-    df = pd.DataFrame(tuples_list, columns=["story_id", "title", "description","url", "pub_date", "article_sentiment", "source_name"])
+    df = pd.DataFrame(tuples_list, columns=[
+                      "story_id", "title", "description", "url", "pub_date", "article_sentiment", "source_name"])
     return df
 
 
 def join_all_reddit_info(conn: connection) -> pd.DataFrame:
     "Function that joins all reddit sources SQL tables"
-    query ="""SELECT * FROM reddit_article"""
+    query = """SELECT * FROM reddit_article"""
     with conn.cursor() as cur:
         cur.execute(query)
         tuples_list = cur.fetchall()
-    df = pd.DataFrame(tuples_list, columns=["article_id", "domain", "title","article_url", "url", "re_sentiment_mean", "re_sentiment_st_dev", "re_sentiment_median", "re_vote_score", "re_upvote_ratio", "re_post_comments", "re_processed_comments", "re_created_timestamp"])
+    df = pd.DataFrame(tuples_list, columns=["article_id", "domain", "title", "article_url", "url", "re_sentiment_mean", "re_sentiment_st_dev",
+                      "re_sentiment_median", "re_vote_score", "re_upvote_ratio", "re_post_comments", "re_processed_comments", "re_created_timestamp"])
     return df
+
 
 def get_db_connection():
     """Establishes a connection with the PostgreSQL database."""
@@ -94,23 +97,26 @@ def create_report(db_connection: connection, stories_data: pd.DataFrame, reddit_
     images within the html wrapper"""
 
     # Sort the stories_data DataFrame by article_sentiment in descending order
-    sorted_article_data = stories_data.sort_values(by='article_sentiment', ascending=False)
+    sorted_article_data = stories_data.sort_values(
+        by='article_sentiment', ascending=False)
 
     top_5_stories_data = sorted_article_data.head(5)
     top_5_stories_titles = top_5_stories_data["title"]
-    
+
     lowest_5_stories_data = sorted_article_data.tail(5)
     lowest_5_stories_titles = lowest_5_stories_data["title"]
 
-    sorted_reddit_data = reddit_data.sort_values(by='re_sentiment_mean', ascending=False)
+    sorted_reddit_data = reddit_data.sort_values(
+        by='re_sentiment_mean', ascending=False)
 
     top_5_reddit_data = sorted_reddit_data.head(5)
     top_5_reddit_titles = top_5_reddit_data["title"]
-    
+
     lowest_5_reddit_data = sorted_reddit_data.tail(5)
     lowest_5_reddit_titles = lowest_5_reddit_data["title"]
 
-    stories_sources_average = stories_data.groupby("source_name")["article_sentiment"].mean().__round__(2)
+    stories_sources_average = stories_data.groupby(
+        "source_name")["article_sentiment"].mean().__round__(2)
 
     if stories_sources_average[0] > 0:
         bbc_line_color = "lightgreen"
@@ -121,29 +127,29 @@ def create_report(db_connection: connection, stories_data: pd.DataFrame, reddit_
         daily_mail_line_color = "lightgreen"
     if stories_sources_average[1] < 0:
         daily_mail_line_color = "#FF7276"
-    
+
     bbc_fig = go.Figure(go.Indicator(
-        mode = "gauge+number+delta",
-        value = stories_sources_average[0],
-        domain = {'x': [0, 1], 'y': [0, 1]},
-        gauge = {'axis': {'range': [-1, 1]},
-                 'bar': {'color': bbc_line_color,
-                         'line': {"color": "white",
-                                  "width": 3}},
-                 'bgcolor': "white"},
-        title = {'text': "BBC Average Sentiment"}))
+        mode="gauge+number+delta",
+        value=stories_sources_average[0],
+        domain={'x': [0, 1], 'y': [0, 1]},
+        gauge={'axis': {'range': [-1, 1]},
+               'bar': {'color': bbc_line_color,
+                       'line': {"color": "white",
+                                "width": 3}},
+               'bgcolor': "white"},
+        title={'text': "BBC Average Sentiment"}))
     bbc_fig.write_image("bbc_plot.svg")
 
     daily_mail_fig = go.Figure(go.Indicator(
-        mode = "gauge+number+delta",
-        value = stories_sources_average[1],
-        domain = {'x': [0, 1], 'y': [0, 1]},
-        gauge = {'axis': {'range': [-1, 1]},
-                 'bar': {'color': daily_mail_line_color,
-                         'line': {"color": "white",
-                                  "width": 3}},
-                 'bgcolor': "white"},
-        title = {'text': "Daily Mail Average Sentiment"}))
+        mode="gauge+number+delta",
+        value=stories_sources_average[1],
+        domain={'x': [0, 1], 'y': [0, 1]},
+        gauge={'axis': {'range': [-1, 1]},
+               'bar': {'color': daily_mail_line_color,
+                       'line': {"color": "white",
+                                "width": 3}},
+               'bgcolor': "white"},
+        title={'text': "Daily Mail Average Sentiment"}))
     daily_mail_fig.write_image("daily_mail_plot.svg")
 
     template = f'''
@@ -154,10 +160,10 @@ def create_report(db_connection: connection, stories_data: pd.DataFrame, reddit_
     /* Define the CSS styles for your dashboard here */
 
     .title-container {{
-        display: flex;
-        flex-direction: column;
+        display: inline-flex;
+        flex-direction: row;
         align-items: center;
-        justify-content: flex-end;
+        justify-content: center;
     }}
     
     .widget {{
@@ -215,6 +221,7 @@ def create_report(db_connection: connection, stories_data: pd.DataFrame, reddit_
 '''
     return template
 
+
 def convert_html_to_pdf(html_template: str, file: str) -> None:
     """Converts the HTML template provided into a pdf report file"""
     # open output file for writing (truncated binary)
@@ -226,8 +233,8 @@ def convert_html_to_pdf(html_template: str, file: str) -> None:
 
     # convert HTML to PDF
     pisa_status = pisa.CreatePDF(
-            html_template,                # the HTML to convert
-            dest=result_file)           # file handle to receive result
+        html_template,                # the HTML to convert
+        dest=result_file)           # file handle to receive result
 
     # close output file
     result_file.close()
@@ -235,15 +242,17 @@ def convert_html_to_pdf(html_template: str, file: str) -> None:
     # return True on success and False on errors
     return pisa_status.err
 
+
 if __name__ == "__main__":
     load_dotenv()
     db_conn = get_db_connection()
-    
+
     joined_stories_df = join_all_stories_info(db_conn)
     joined_stories_df.to_csv("testing_stories_join.csv", index=False)
 
     joined_reddit_df = join_all_reddit_info(db_conn)
     joined_reddit_df.to_csv("testing_reddit_join.csv", index=False)
 
-    report_template = create_report(db_conn, joined_stories_df, joined_reddit_df)
+    report_template = create_report(
+        db_conn, joined_stories_df, joined_reddit_df)
     convert_html_to_pdf(report_template, environ.get("REPORT_FILE"))
